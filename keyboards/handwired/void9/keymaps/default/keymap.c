bf8af7ac8d91edcc340ceb9800255a9f5a45c410
Jacob Gibbs <jacob89gibbs@gmail.com>
2022-01-20 20:08:03 +0000

    Updating keymapping

--- keyboards/handwired/void9/keymaps/default/keymap.c
+++ keyboards/handwired/void9/keymaps/default/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `--------------------'
  */
 [_BASE] = LAYOUT_ortho_3x3(
-    KC_7,       KC_8,       KC_9,
-    KC_4,       KC_5,       KC_6,
-    KC_1,       KC_2,       KC_3
+    KC_9,       KC_8,       KC_7,
+    KC_6,       KC_5,       KC_4,
+    LGUI(KC_X),       LGUI(KC_V),       LGUI(KC_C)
 ),
 
 /* FN1
